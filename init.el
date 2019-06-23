70729366cc28b9d5466eb2c47f3f77f4b6f674e9
Roland Everaert <r.everaert@gmail.com>
2019-06-23 19:18:35 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,30 @@
+;; -*- eval: (git-auto-commit-mode 1) -*-
+;; This configuration is inspired heavily from Mike Zamansky's blog
+;; http://cestlaz.github.io/stories/emacs/
+
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+(require 's)
+(require 'f)
+;; (require 'ht)
+(require 'git)
+(require 'ert)
+(require 'use-package)
+
+(setq default-directory (f-full (getenv "HOME")))
+
+(defun load-local (file)
+  (load (f-expand file user-emacs-directory)))
+
+(dolist module (f-files "modules" ))
+
+
+
+
+
+
+;; (load-local "mods")
+
+;; (dolist elisp-mod reve:mods
+;;         (load-local elisp-mod))
